7de97fd520eaa249ffbff64b2b1dffd03a4faa10
Paulo Gonçalves <paulorochag@hotmail.com>
2020-02-23 20:49:42 -0300

    refactor(cartao): reutilização de código para melhor manutenibilidade

--- src/conteudo/cartao.js
+++ src/conteudo/cartao.js
@@ -3,43 +3,24 @@
 const chalk = require('chalk')
 
 const { cinza, novaLinha } = require('../constants_and_utils/constants')
+const { quantidadeDeEspaco } = require('../constants_and_utils/utils')
 
-const dados = {
-  nome: '             ' + chalk.white.bold('Paulo Gonçalves'),
-  trabalho: chalk.white('             Quality Assurance'),
-  email: chalk.rgb(cinza.red, cinza.green, cinza.blue)('paulorochag@hotmail.com'),
-  github: chalk.rgb(cinza.red, cinza.green, cinza.blue)('https://github.com/paulogoncalvesbh'),
-  linkedin: chalk.rgb(cinza.red, cinza.green, cinza.blue)('https://linkedin.com/in/paulo-goncalves'),
-  devto: chalk.rgb(cinza.red, cinza.green, cinza.blue)('https://dev.to/paulogoncalvesbh'),
-  twitter: chalk.rgb(cinza.red, cinza.green, cinza.blue)('https://twitter.com/paulorgoncalves'),
-  npx: chalk.white('             npx paulogoncalves [-h | --help]'),
-  labelEmail: chalk.blue.bold('     Email:'),
-  labelGitHub: chalk.green.bold('    GitHub:'),
-  labelLinkedIn: chalk.red.bold('  LinkedIn:'),
-  labelDevto: chalk.yellow.bold('    Dev.to:'),
-  labelTwitter: chalk.cyan.bold('   Twitter:')
+const valores = texto => {
+  return `  ${chalk.rgb(cinza.red, cinza.green, cinza.blue)(texto)}${novaLinha}`
 }
 
-const heading = `${dados.nome}`
-const working = `${dados.trabalho}`
-const emailing = `${dados.labelEmail}  ${dados.email}`
-const githubing = `${dados.labelGitHub}  ${dados.github}`
-const linkedining = `${dados.labelLinkedIn}  ${dados.linkedin}`
-const devtoing = `${dados.labelDevto}  ${dados.devto}`
-const twittering = `${dados.labelTwitter}  ${dados.twitter}`
-const carding = `${dados.npx}`
-
-const conteudo = heading + novaLinha +
-               novaLinha +
-               working + novaLinha +
-               novaLinha +
-               emailing + novaLinha +
-               githubing + novaLinha +
-               linkedining + novaLinha +
-               devtoing + novaLinha +
-               twittering + novaLinha +
-               novaLinha +
-               carding
+const conteudo =
+  quantidadeDeEspaco(13) + chalk.white.bold('Paulo Gonçalves') + novaLinha +
+  novaLinha +
+  quantidadeDeEspaco(13) + chalk.white('Quality Assurance') + novaLinha +
+  novaLinha +
+  chalk.blue.bold('     Email:') + valores('paulorochag@hotmail.com') +
+  chalk.green.bold('    GitHub:') + valores('https://github.com/paulogoncalvesbh') +
+  chalk.red.bold('  LinkedIn:') + valores('https://linkedin.com/in/paulo-goncalves') +
+  chalk.yellow.bold('    Dev.to:') + valores('https://dev.to/paulogoncalvesbh') +
+  chalk.cyan.bold('   Twitter:') + valores('https://twitter.com/paulorgoncalves') +
+  novaLinha +
+  quantidadeDeEspaco(13) + chalk.white('npx paulogoncalves [-h | --help]')
 
 module.exports = {
   conteudo,
